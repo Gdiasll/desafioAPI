2e33936cc006a1d052ed069904b56ef18d8424fd
Gdiasll <gdiasll@hotmail.com>
2019-02-12 14:39:19 -0200

    Desenvolvimento da função de obter um usuário, e prototipo de proxima função

--- api/controllers/usuario.js
+++ api/controllers/usuario.js
@@ -5,7 +5,9 @@ const urlConexao = require('../../config/pg_connection');
 
 module.exports = {
     cadastrar_usuario: cadastrarUsuario,
-    atualizar_usuario: atualizarUsuario
+    atualizar_usuario: atualizarUsuario,
+    obter_usuario: obterUsuario,
+    obter_todos: obterTodos
 };
 
 async function cadastrarUsuario(req, res) {
@@ -41,11 +43,35 @@ async function atualizarUsuario(req, res) {
 
         await client.query('UPDATE usuario SET (nome, data_nascimento, cpf) = ($1, $2, $3) WHERE id = ($4)', [nome, data_nascimento, cpf, id[0]]);
         await client.end();
-        console.log(req)
+
         res.status(201).json( { mensagem: 'Usuario atualizado com sucesso!' } );
         
     } catch (err) {
         console.log(err);
         res.status(500).json( { mensagem: err.toString() } );
     }
+}
+
+// Função responsável por obter um usuário existente
+async function obterUsuario(req, res) {
+    const id_usuario = req.swagger.params.usuario_id.value;
+
+    try {
+        const client = new Client({ connectionString: urlConexao });
+        await client.connect();
+
+        const usuario = await client.query(`SELECT nome, data_nascimento, cpf FROM usuario WHERE id =${id_usuario}`);
+        await client.end();
+
+        res.status(201).send(usuario.rows[0])
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json( { mensagem: err.toString() } );
+    }
+}
+
+// Funçao para obter todos usuarios
+async function obterTodos(req, res) {
+    // ------------//
 }
