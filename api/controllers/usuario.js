a63e77cca28da62d8dd2acdb70e4c1c6c1731084
Gdiasll <gdiasll@hotmail.com>
2019-02-12 11:30:39 -0200

    Implementação da função PUT, que atualiza o usuario existente

--- api/controllers/usuario.js
+++ api/controllers/usuario.js
@@ -4,7 +4,8 @@ const { Client } = require('pg');
 const urlConexao = require('../../config/pg_connection');
 
 module.exports = {
-    cadastrar_usuario: cadastrarUsuario
+    cadastrar_usuario: cadastrarUsuario,
+    atualizar_usuario: atualizarUsuario
 };
 
 async function cadastrarUsuario(req, res) {
@@ -20,6 +21,28 @@ async function cadastrarUsuario(req, res) {
         await client.end();
 
         res.status(201).json( { mensagem: 'Usuário cadastrado com sucesso!' } );
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json( { mensagem: err.toString() } );
+    }
+}
+
+// Função responsavel por atualizar um usuario existente
+async function atualizarUsuario(req, res) {
+    const id = req.swagger.params.usuario_id;
+    const nome = req.body.nome.trim();
+    const data_nascimento = req.body.data_nascimento;
+    const cpf = req.body.cpf;
+
+    try {
+        const client = new Client({ connectionString: urlConexao });
+        await client.connect();
+
+        await client.query('UPDATE usuario SET (nome, data_nascimento, cpf) = ($1, $2, $3) WHERE id = ($4)', [nome, data_nascimento, cpf, id[0]]);
+        await client.end();
+        console.log(req)
+        res.status(201).json( { mensagem: 'Usuario atualizado com sucesso!' } );
         
     } catch (err) {
         console.log(err);
